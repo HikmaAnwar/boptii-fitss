21c8eb5cab5f12c524a205ac236dcbdf66898e7f
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-07-22 21:52:29 +0300

    feat: added nav bar component

--- src/components/nav/page.js
+++ src/components/nav/page.js
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
 const navBar = () =>{
-    <nav className=""
+    <nav className=" flex justify-between items-center bg-white rounded-md p-6 text-black mx-auto w-full">
+        <div className=" text-black font-bold no-underline">
+            <Link href="/">Boptii Fits</Link>
+            </div>
+    </nav>
 }
