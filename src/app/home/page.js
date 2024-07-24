814855b5614a52d5addb9aedc9e70a707c472a09
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-07-24 17:21:31 +0300

    fix: minor change

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -9,8 +9,7 @@ const Home = () => {
       <div className="absolute inset-0 flex flex-col items-center justify-center pt-24">
         <p className="text-black mx-auto font-bold text-4xl font-serif mt-8 mb-6">VIRTUAL WARDROBE STYLING</p>
         <Link href="https://www.instagram.com/boptii/?hl=en" target="blank" className="text-black underline italic mt-4 my-4">By Hikma Anwar</Link>
-        <button className="bg-brown-dark text-white p-3 pl-6 pr-6 pt-4 pb-4 rounded-3xl text-md my-6">Get Styled</button>
-        
+       <Link href="/services"> <button className="bg-brown-dark text-white p-3 pl-6 pr-6 pt-4 pb-4 rounded-3xl text-md my-6">Get Styled</button></Link>
       </div>
     </div>
   );
