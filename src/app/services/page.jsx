6c91d2f786b1797c0c4b1ed1ee3a84152d08f513
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-09-04 18:56:37 +0300

    fix: minor change

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -8,10 +8,20 @@ const Services = () => {
         <h2 className="flex mb-6 min-h-screen justify-center items-center text-black text-3xl">
           Styling Services
         </h2>
-        <hr className="border-t border-gray-300 mb-6" />
-        <div className="flex justify-center space-x-8 mb-6">
+      </div>
+      <hr className="border-t border-gray-300 mb-6" />
+      <div className="flex justify-center space-x-8 mb-12">
+        <div>
           <Image src="/assets/women-lookbook.jpg" width={500} height={100} />
+          <p className="text-black text-xl mt-2">
+            Women's Personalized Lookbook
+          </p>
+          <p className="text-black text-md mt-2  mb-8">from $150.00</p>
+        </div>
+        <div>
           <Image src="/assets/men-lookbook.jpg" width={500} height={100} />
+          <p className="text-black text-xl mt-2">Men's Personalized Lookbook</p>
+          <p className="text-black text-md mt-2  mb-8">from $150.00</p>
         </div>
       </div>
     </div>
