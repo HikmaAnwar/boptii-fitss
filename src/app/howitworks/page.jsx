85b2235409db7dae3e57d827f61d6ab326b6df03
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-09-04 15:28:15 +0300

    fix: minor change

--- src/app/howitworks/page.jsx
+++ src/app/howitworks/page.jsx
@@ -1,20 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
+
 const HowItWorks = () => {
   return (
-    <div className="bg-white w-full h-full">
-      <p className=" font-semibold font-san my-6 text-black text-center text-4xl pt-52 pb-14 ">
+    <div className="bg-white w-full min-h-screen">
+      <p className="font-semibold font-san my-6 text-black text-center text-4xl pt-52 pb-8">
         HOW IT WORKS
       </p>
-      <div className="relative h-screen w-full flex space-x-4">
-        <div className="bg-colors-broken-white p-4 rounded-lg shadow-lg max-w-md  flex-1">
+      <div className="relative w-full flex space-x-4">
+        <div className="bg-colors-broken-white p-4 rounded-lg shadow-lg max-w-md flex-1">
           <Image
             src="/assets/check-list.jpg"
             alt="images"
             width={500}
             height={100}
           />
-          <h3 className="text-xl text-center text-black font-serif items-center font-semibold">
+          <h3 className="text-xl text-center text-black font-serif font-semibold">
             STEP 1: STYLE QUIZ & PAYMENT
           </h3>
           <p className="mt-2 text-center text-black font-san">
@@ -24,7 +25,7 @@ const HowItWorks = () => {
             check out and place your order.
           </p>
         </div>
-        <div className="bg-colors-broken-white  p-4 rounded-lg shadow-lg max-w-sm flex-1">
+        <div className="bg-colors-broken-white p-4 rounded-lg shadow-lg max-w-sm flex-1">
           <Image src="/assets/card.jpg" alt="images" width={500} height={100} />
           <h3 className="text-xl text-center text-black font-serif font-semibold">
             STEP 2: WAITING PERIOD
@@ -32,8 +33,8 @@ const HowItWorks = () => {
           <p className="mt-2 text-center text-black font-san">
             Once your order is processed, you may email us inspo pictures or
             products you already own that you would like to see styled in your
-            lookbook. Our stylists will start creating your Lookbook and send
-            within the delivery time you chose! .
+            Lookbook. Our stylists will start creating your Lookbook and send
+            within the delivery time you chose!
           </p>
         </div>
         <div className="bg-colors-broken-white p-4 rounded-lg shadow-lg max-w-sm flex-1">
@@ -47,10 +48,10 @@ const HowItWorks = () => {
             STEP 3: RECEIVAL & DIRECT SHOPPING
           </h3>
           <p className="mt-2 text-center text-black font-san">
-            Receive your Personalized Loobook via your preferred email and start
-            shopping for your outfits directly from your Lookbook! All styled
-            pieces in your Lookbook will be linked, enabling you to easily click
-            and shop.
+            Receive your Personalized Lookbook via your preferred email and
+            start shopping for your outfits directly from your Lookbook! All
+            styled pieces in your Lookbook will be linked, enabling you to
+            easily click and shop.
           </p>
         </div>
         <div className="bg-colors-broken-white p-4 rounded-lg shadow-lg max-w-sm flex-1">
@@ -61,19 +62,18 @@ const HowItWorks = () => {
             height={100}
           />
           <h3 className="text-xl font-semibold font-serif text-center text-black">
-            STEP 3: RECEIVAL & DIRECT SHOPPING
+            STEP 4: STYLE YOUR OUTFIT
           </h3>
           <p className="mt-2 text-center text-black font-san">
-            Receive your Personalized Loobook via your preferred email and start
-            shopping for your outfits directly from your Lookbook! All styled
-            pieces in your Lookbook will be linked, enabling you to easily click
-            and shop.
+            Now that you have your personalized Lookbook, you can start styling
+            your outfit! Each piece is carefully selected to match your
+            preferences, making your shopping experience seamless.
           </p>
         </div>
       </div>
-      <div className="flex items-center justify-center min-h-screen ">
+      <div className="flex items-center justify-center  my-12">
         <Link href="/services">
-          <button className="bg-brown-dark text-white p-3 pl-6 pr-6 pt-4 pb-4 rounded-3xl text-md my-2">
+          <button className="bg-brown-dark text-white p-3 pl-6 pr-6 pt-4 pb-4 rounded-3xl text-md">
             Get Styled
           </button>
         </Link>
@@ -81,4 +81,5 @@ const HowItWorks = () => {
     </div>
   );
 };
+
 export default HowItWorks;
