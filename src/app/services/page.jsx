121c2b621459edf35dee74f03c9e666fcfd65448
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-09-06 06:53:00 +0300

    feat: added some contents to the page

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -4,10 +4,15 @@ import Link from "next/link";
 const Services = () => {
   return (
     <div className="bg-white px-4 py-8 sm:py-16 lg:px-8 h-[1000px]">
-      <div className="text-center mb-12">
-        <h2 className="text-3xl font-semibold text-gray-800 sm:text-4xl">
+      <div className="text-center mt-36 mb-12">
+        <h2 className="text-3xl font-semibold  text-gray-800 sm:text-4xl">
           Styling Services
         </h2>
+        <div className="mt-14 text-lg text-gray-600">
+          <span>Women</span>
+          <span className="mx-6">|</span>
+          <span>Men</span>
+        </div>
       </div>
       <hr className="border-t border-gray-300 mb-12" />
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 max-w-5xl mx-auto">
