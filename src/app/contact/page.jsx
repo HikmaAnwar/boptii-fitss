eff94e581ab5586afcccb2bccac066eaf3bfd01e
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-09-07 09:41:09 +0300

    fix: fixed the layout

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState } from "react";
+
+const Contact = () => {
+  return (
+    <div className="bg-white ">
+      <div className=" flex flex-col lg:flex-row">
+        <div className="mt-56 ml-8 mb-10 font-serif text-black w-1/2">
+          <h1 className="text-black text-5xl font-normal">Contact Us</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Contact;
--- src/app/layout.js
+++ src/app/layout.js
@@ -13,10 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <NavBar/>
-        {children}</body>
-        <Footer/>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
+        <NavBar />
+        <main className="flex-grow">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
