79564b18e6133b0673e9f9d9a9be20771f0e041f
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-09-07 17:05:49 +0300

    fix: minor change

--- src/components/lookbook-page.jsx
+++ src/components/lookbook-page.jsx
@@ -90,7 +90,6 @@ const WomensPersonalizedLookbook = () => {
                 </select>
               </div>
 
-              {/* Button to open the modal */}
               <button
                 onClick={openModal}
                 className="bg-brown-dark w-full sm:w-2/3 text-white p-6 rounded-3xl text-lg my-6"
@@ -102,11 +101,9 @@ const WomensPersonalizedLookbook = () => {
         </div>
       </div>
 
-      {/* Modal */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
           <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full relative">
-            {/* Close button */}
             <button
               onClick={closeModal}
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
@@ -114,7 +111,6 @@ const WomensPersonalizedLookbook = () => {
               &times;
             </button>
 
-            {/* Quiz Content */}
             <h2 className="text-xl font-bold mb-4">Style Quiz</h2>
             <form>
               <div className="mb-4">
@@ -136,7 +132,7 @@ const WomensPersonalizedLookbook = () => {
               <div className="flex justify-end">
                 <button
                   type="submit"
-                  className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
+                  className="bg-brown-dark text-white py-2 px-4 rounded-lg hover:bg-brown-light"
                 >
                   Submit Quiz
                 </button>
