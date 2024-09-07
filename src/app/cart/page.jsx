e53051eec88bb10deb46914a7358aed5dc10da04
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-09-07 13:52:50 +0300

    fix; minor change

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -11,15 +11,15 @@ const CartPage = () => {
   };
 
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl text-black mt-48 font-serif ml-8 mb-4">
+    <div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
+      <h1 className="text-3xl text-black mt-44 font-serif ml-4 sm:ml-8 mb-4">
         Shopping Cart
       </h1>
       {cartItems.length > 0 ? (
         <div>
           <ul className="space-y-4">
             {cartItems.map((item, index) => (
-              <li key={index} className="p-4 border rounded shadow-md">
+              <li key={index} className="p-4 border rounded-lg shadow-md">
                 <h2 className="text-xl font-semibold">{item.name}</h2>
                 <p>{item.description}</p>
                 <p className="font-bold mt-2">${item.price}</p>
@@ -28,11 +28,11 @@ const CartPage = () => {
           </ul>
         </div>
       ) : (
-        <div className="text-gray-700 px-8 py-4 font-serif">
+        <div className="text-gray-700 px-4 sm:px-8 py-4 font-serif">
           <p className="text-lg">You have nothing in your shopping cart.</p>
           <button
             onClick={handleContinueShopping}
-            className="mt-4 bg-brown-dark text-white text-lg py-4 px-6 my-4 rounded-3xl hover:bg-brown-light transition"
+            className="mt-4 bg-brown-dark text-white text-lg py-3 px-4 sm:py-4 sm:px-6 rounded-xl hover:bg-brown-light transition"
           >
             Continue Shopping
           </button>
